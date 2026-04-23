54ea9fa0f668fa5b3407f802459b412e7a72b18b
Muh Arya Saputra <aryaom74@gmail.com>
2026-04-23 19:58:30 +0700

    Refactor LinkedList implementation in C++

--- 0071_SingleLinkList.cpp
+++ 0071_SingleLinkList.cpp
@@ -175,5 +175,15 @@ int main()
             }
             break;
         }
+            case '5':
+            cout << "\nKeluar program...\n";
+            break;
 
-        
+        default:
+            cout << "\nPilihan tidak valid!\n";
+        }
+
+    } while (pilihan != '5');
+
+    return 0;
+}
